4bf760998c4f9facca233c3517d1208af627e63e
Grinchcreator <119819551+Grinchcreator@users.noreply.github.com>
2023-05-08 23:26:32 +0300

    feat: add task 4 solution (08.05.2023)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+const files = path.join(__dirname, 'files');
+const filesCopy = path.join(__dirname, 'files-copy');
 
+fs.rm(filesCopy, {recursive: true}, () => {
+    if (err) {
+        throw err;
+    }
+});
+
+fs.mkdir(filesCopy, {recursive: true}, err => {
+    if (err) {
+        throw err;
+    }
+});
+
+fs.readdir(files, (err, files) => {
+    if (err) {
+        throw err;
+    }
+
+files.forEach((file) => {
+    const filePath = path.join(__dirname, 'files', file.name);
+    const copyFilePath = path.join (__dirname, 'files-copy', file.name);
+    fs.copyFile (filePath, copyFilePath);
+  });
+});
